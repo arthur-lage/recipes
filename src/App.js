2ee80ce3d008819425e4162f9007ee86c93b87a5
Arthur Lage <arthurlage2006@gmail.com>
2021-10-23 23:05:45 -0300

    login page with responsivity (not done yet)

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
-import React from 'react'
+import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Login from './pages/Login'
+import Login from "./pages/Login";
+
+import "./globalStyles.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Switch>
-        <Route path="/" exact component={Login}/>
+        <Route path="/" exact component={Login} />
       </Switch>
     </BrowserRouter>
   );
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import { Container } from './style'
+import React from "react";
+import "./style.css";
+
+import IllustrationImage from '../../assets/images/login-illustration.svg'
+
+import GoogleIcon from '../../assets/images/google-icon.svg'
+import GithubIcon from '../../assets/images/github-icon.svg'
 
 function Login() {
-    return (
-        <Container>
-            <h1>Login</h1>
-        </Container>
-    )
+  return (
+    <div className="login-container">
+      <main className="login-main-container">
+        <div className="login-left-content">
+            <img src={IllustrationImage} alt="Illustration-Cooking" className="login-illustration" />
+        </div>
+
+        <div className="login-right-content">
+            <h1 className="login-recipes-title">RECIPES</h1>
+            <h2 className="login-login-title">LOGIN</h2>
+
+            <button className="login-login-with-google login-btn">
+                <img src={GoogleIcon} alt="Google-Icon" />
+                Connect with Google
+            </button>
+
+            <button className="login-login-with-github login-btn">
+                <img src={GithubIcon} alt="Github-Icon" />
+                Connect with GitHub
+            </button>
+        </div>
+      </main>
+
+      <div className="login-footer">
+        <p className="login-footer-paragraph">
+          &copy;2021 - Made with ❤️ by <b>Arthur Lage</b>
+        </p>
+      </div>
+    </div>
+  );
 }
 
-export default Login
+export default Login;
